c38dd67940af76746830157edfbf4d3f77475ccc
Pablo Gutierrez <111473283+PabloYR16@users.noreply.github.com>
2023-05-09 23:19:22 -0300

    modificación de los archivos correspondientes

--- Python/Semana2/leer_archivo.py
+++ Python/Semana2/leer_archivo.py
@@ -1,7 +1,19 @@
-archivo = open ('prueba.txt', 'r',encoding="utf8")
-#print(archivo.read())
+archivo = open('prueba.txt', 'r',
+               encoding='utf8')  # las letras son: 'r' read, 'a' append, 'w' write, 'x'
+# print(archivo.read())
+# print(archivo.read(16))
+# print(archivo.read(10)) # Continuamos desde la linea anterior
+# print(archivo.readline())
+# print(archivo.readline())
 
-print(archivo.read(16))
-print(archivo.read(10)) #continuamos desde la linea anterior
-print (archivo.readline())
+# vamos a iterar el archivo, cada una de las lineas
+# for linea in archivo:
+# print(linea): iteramos todos los elementos del archivo
+# print(archivo.readlines()[11])  # accedemos al archivo como si fuera una lista
+# Anexamos información, copiamos a otro
+archivo2 = open('copia.txt', 'w', encoding='utf8')
+archivo2.write(archivo.read())
+archivo.close()  # cerramos el primer archivo
+archivo2.close()  # cerramos el segundo archivo
 
+print('Se ha terminado el proceso de leer y copiar archivos')

9f003a9d03317059405ae22ba451ba8e6df5b235
Angel Escobar <111505818+angesc2022@users.noreply.github.com>
2023-05-14 23:12:25 -0300

    02-03 arreglos.Js
    subo la parte restante que es un array semana 2 Js

--- Javascript/Semana 2/02-03-arreglos.js
+++ Javascript/Semana 2/02-03-arreglos.js
@@ -1,20 +1,33 @@
-//let autos = new Array('Ferrari', 'Renault', 'BMW'); esta es la sintaxis vieja
+//let autos = new Array['Ferrari', 'Renault', 'BMW']; ESTA ES LA SINTAXIS VIEJA
 const autos = ['Ferrari', 'Renault', 'BMW'];
 console.log(autos);
 
-//Modificamos los elementos del arreglo 
-autos[1] = "volvo"
-console.log(autos[1]),
+console.log(autos[0]);
+console.log(autos[2]);
 
+for(let i = 0; i < autos.length; i++){
+    console.log(i+' : '+autos[i]);
+}
 
-//Agregamos nuevos valores al arreglo 
-autos.push("Audi");
+//Modificamos los elementos del arreglo
+autos[1] = 'Volvo';
+console.log(autos[1]);
+
+//Agregamos nuevos valores al arreglo
+autos.push('Audi');
 console.log(autos);
 
-//otras formas de agregar elementos al arreglo 
-autos[autos.length] = 'porche';
+//Otras formas de agregar elementos al arreglo
+autos[autos.length] = 'Porche';
 console.log(autos);
 
-//Tercera forma de agregar elementos teniendo cuidado
-autos[5] = "Renault";
+//Tercera forma de agregar elementos teniendo CUIDADO
+autos[6] = 'Renault';
 console.log(autos);
+
+
+//Como preguntar si es un Arrray o Arreglo
+console.log(Array.isArray(autos)); //Dvuelve un booleano
+
+console.log(autos instanceof Array); //Preguntamos si la variable es una instancia de la clase Array
+

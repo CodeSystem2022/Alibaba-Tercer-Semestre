ab596a69273766840035f9705948bc6c034b408a
Jorge Huaman Pérez <jorgejheik@gmail.com>
2023-05-15 23:06:05 -0300

    Semana 3

--- Javascript/Semana 3/01-03-funciones.js
+++ Javascript/Semana 3/01-03-funciones.js
@@ -16,7 +16,7 @@ let x = function(a, b){ return a + b}; //necesita cierre con punto y coma
 resultado = x(5, 6); //al llamarla se pone la variable y parentesis
 console.log(resultado);
 
-//Funcines de tipo self y invoking
+//Funciones de tipo self y invoking
 (function(a, b){
     console.log('Ejecutando la funcion: '+(a + b));
 })(9, 6);

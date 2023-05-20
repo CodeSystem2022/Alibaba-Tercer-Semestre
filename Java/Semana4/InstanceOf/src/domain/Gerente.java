4666ea12e6fd4c4f1da2a797404c873c1b7fa6ed
JuanIgnaMurua <juanigm@gmail.com>
2023-05-20 14:15:16 -0300

    Se sube el proyecto de Java- InstanceOf

---
+++ Java/Semana4/InstanceOf/src/domain/Gerente.java
@@ -0,0 +1,29 @@
+
+package domain;
+
+public class Gerente extends Empleado {
+    private String departamento;
+
+    public Gerente(String nombre,double sueldo,String departamento){
+        super(nombre,sueldo);
+        this.departamento=departamento;
+
+    }
+
+    @Override // sobreescrimos el metodo
+    public String obtenerDetalles(){
+        return super.obtenerDetalles() + ", Departamento "+ this.departamento;
+
+
+    }
+
+    public String getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(String departamento) {
+        this.departamento = departamento;
+    }
+    
+}
+
---
+++ Java/Semana4/InstanceOf/src/test/TestInstanceOf.java
@@ -0,0 +1,21 @@
+
+package test;
+
+import domain.*;
+
+public class TestInstanceOf {
+    public static void main(String[] args) {
+
+        Empleado empleado1= new Empleado("Juan",10000);
+        imprimir(empleado1);
+        //System.out.println("empleado1 = " + empleado1.obtenerDetalles());
+        empleado1 = new Gerente("Jose",5000,"Sistemas");
+        imprimir(empleado1);
+        //System.out.println("gerente1 = " + gerente1.obtenerDetalles());
+    }
+    public static void imprimir(Empleado empleado){
+        String detalles= empleado.obtenerDetalles();
+        System.out.println("detaalles" + detalles);
+    }
+    
+}

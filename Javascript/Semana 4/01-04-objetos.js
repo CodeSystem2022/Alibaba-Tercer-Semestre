cc624b5f4b3c399a5671d252ef20ff6765f8119b
Angel Escobar <111505818+angesc2022@users.noreply.github.com>
2023-05-20 18:01:51 -0300

    tarea realizada.
    JS-Modificar y agregar al archivo 01-04-objectos.js, Ejecutamos desde el navegador, Distintas formas de imprimir un Objeto con: Object.values() y JSON.stringify()

--- Javascript/Semana 4/01-04-objetos.js
+++ Javascript/Semana 4/01-04-objetos.js
@@ -32,5 +32,30 @@ for(propiedad in persona){
     console.log(propiedad);
     console.log(persona[propiedad]);
 }
+console.log('Cambiamos y eliminamos un error');
+persona.apellida = 'Betancud'; // cambiamos dianmicamente un valor del  objeto
+delete persona.apellida;// Eliminamos el error
+console.log(persona);
+
+//Distintas formas de imprimir un objeto
+// Numero 1: la mas sencilla : concatenar cada  valor de cada propiedad
+console.log(persona.nombre+','+persona.apellido);
+console.log('Distintas formas de imprimir un objeto: forma 1');
+
+// Numero 2: A travez del coclo for in 
+console.log('Distintas formas de imprimir un objeto: forma 2');
+for(nombrePropiedad in persona){
+    console.log(nombrePropiedad);
+        console.log(persona[nombrePropiedad]);
+}
+// Numero 3: La funcion Objeto.Values()
+console.log('Distintas formas de imprimir un objeto: forma 3');
+let personaArray = Object.values(persona);
+console.log(personaArray);
+
+// Numero 4: Utilizamos el metodo JSON.stringsify
+console.log('Distintas formas de imprimir un objeto forma 4');
+let personaString = JSON.stringify(persona);
+console.log(personaString);
 
 

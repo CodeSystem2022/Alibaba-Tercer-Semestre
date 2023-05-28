19af993b904786de6add7e81f166b13ea2dd5c7f
Gabriel <63930189+Gabibelt@users.noreply.github.com>
2023-05-28 15:09:09 -0300

    Handler
    Actualizacion hasta handler

--- Python/Semana 7/capa_datos_persona/logger_base.py
+++ Python/Semana 7/capa_datos_persona/logger_base.py
@@ -1,4 +1,10 @@
 import logging as log
 
 # Llamamos una configuración básica
-log.basicConfig(level=log.DEBUG)
+log.basicConfig(level=log.DEBUG,
+                format='%(asctime)s:%(levelname)s [%(filename)s:$(lineo)s] %(message)s',
+                datefmt='%I:%M:%S %p',
+                handlers=[
+                    log.FileHandler('capa_datos.log'),
+                    log.StreamHandler()
+                ])

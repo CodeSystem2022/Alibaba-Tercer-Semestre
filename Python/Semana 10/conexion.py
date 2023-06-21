37dd18d2050ebb209281444e83c04a590c29d963
Sebastián Lerotich <seba.lerotich@gmail.com>
2023-06-20 22:39:01 -0300

    Se actualiza el archivo conexion.py ya que no habian impactado los cambios.

--- Python/Semana 10/conexion.py
+++ Python/Semana 10/conexion.py
@@ -1,4 +1,4 @@
-import psycopg2 as bd
+from psycopg2 import pool
 # psycopg2 as db otra manera de importar el psycopg2
 from logger_base import log
 import sys
@@ -9,41 +9,37 @@ class Conexion:
     _PASSWORD = 'admin'
     _BD_PORT = '5432'
     _HOST = '127.0.0.1'
-    _conexion = None
-    _cursor = None
+    _MIN_CON = 1
+    _MAX_CON = 5
+    _pool = None
 
     @classmethod
     def obtenerConexion(cls):
-        if cls._conexion is None:
-            try:
-                cls._conexion =  bd.connect(host=cls._HOST,
-                                            user=cls._USERNAME,
-                                            password=cls._PASSWORD,
-                                            port=cls._BD_PORT,
-                                            database=cls._DATABASE)
-                log.debug(f'Conexion Exitosa: {cls._conexion}')
-                return cls._conexion
-            except Exception as e:
-                log.error(f'Ocurrio un error: {e}')
-                sys.exit()
-        else:
-            return cls._conexion
+        pass
 
 
 
     @classmethod
     def obtenerCursor(cls):
-        if cls._cursor is None:
+        pass
+
+    @classmethod
+    def obtenerPool(cls):
+        if cls._pool is None:
             try:
-                cls._cursor = cls.obtenerConexion().cursor()
-                log.debug(f'Se abrio correctamente el cursor: {cls._cursor}')
-                return cls._cursor
+                cls._pool = pool.SimpleConnectionPool(cls._MIN_CON,
+                cls._MAX_CON,
+                host=cls._HOST,
+                user=cls._USERNAME,
+                password=cls._PASSWORD,
+                port=cls._DB_PORT,
+                database=cls._DATABASE)
+                log.debug(f'creación del pool exitosa: {cls._pool}')
             except Exception as e:
-                log.error(f'Ocurrio un error: {e}')
+                log.error(f'Ocurrió un error al obtener el pool: {e}')
                 sys.exit()
         else:
-             return cls._cursor
+            return cls._pool
 
 if __name__ == ' __main__ ':
-    Conexion.obtenerConexion()
-    Conexion.obtenerCursor()
+    pass

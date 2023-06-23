fb2cb29ae8b75c70e07704c8994eb5120526d5d7
lokywolf2295 <matiasesek1995@gmail.com>
2023-06-22 23:12:22 -0300

    Update(Java): Add Switch method with the code

--- Java/Semana10/Leccion11/ListarPersonas/src/ListadoPersonaApp.java
+++ Java/Semana10/Leccion11/ListarPersonas/src/ListadoPersonaApp.java
@@ -32,6 +32,38 @@ public class ListadoPersonaApp {
     }//Fin método mostrarMenu
 
     private static boolean ejecutarOperacion(Scanner entrada, List<Persona> personas){
-
+        var opcion = Integer.parseInt(entrada.nextLine());
+        var salir = false;
+        //Revisamos la opción digitada a través de un switch
+        switch (opcion){
+            case 1 -> {
+                //Agregar una persona a la lista
+                System.out.print("Digite el nombre de la persona: ");
+                var nombre = entrada.nextLine();
+                System.out.print("Digite el telefono de la persona: ");
+                var tel = entrada.nextLine();
+                System.out.print("Digite el correo de la persona: ");
+                var email = entrada.nextLine();
+                //Creamos el objeto persona
+                var persona = new Persona(nombre, tel, email);
+                //Agregamos la persona a la lista
+                personas.add(persona);
+                System.out.println("La lista tiene " + personas.size() + " elementos");
+            } //Fin del caso 1
+            case 2 -> {
+                //Listamos las personas
+                System.out.println("Listado de personas");
+                //Mejoras con lamda y el método de referencia
+                //personas.forEach((persona) -> System.out.println(persona));
+                personas.forEach(System.out::println);
+            } //Fin del caso 2
+            case 3 -> {
+                //Salimos del ciclo
+                System.out.println("Gracias por usar el programa");
+                salir = true;
+            } //Fin del caso 3
+            default -> System.out.println("Opción Incorrecta " + opcion);
+        }//Fin del switch
+        return salir;
     }//fin del método ejecutarOperacion
 }//Fin de la clase ListadoPersonasApp

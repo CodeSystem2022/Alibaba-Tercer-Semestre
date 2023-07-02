9f9322a593751109b209045d68b08ba9797ed52e
lokywolf2295 <matiasesek1995@gmail.com>
2023-07-01 22:17:18 -0300

    Update(Python) add method exit

--- Python/Semana 11/cursor_del_pool.py
+++ Python/Semana 11/cursor_del_pool.py
@@ -11,3 +11,14 @@ class CursorDelPool:
         self._conexion = Conexion.obtenerConexion()
         self._cursor = self._conexion.cursor()
         return self._cursor
+
+    def __exit__(self, tipo_exeption, valor_exeption, detalle_exeption):
+        log.debug(' Se ejecuta el método exit')
+        if valor_exeption:
+            self._conexion.rollback()
+            log.debug(f'Ocurrio una exepción: {valor_exeption}')
+        else:
+            self._conexion.commit()
+            log.debug('commit de la transacción')
+        self._cursor.close()
+        Conexion.liberarConexion(self._conexion)

7b4c244e347587700683686c01438ae7f789370c
Denise  Germiniani <111541137+DenuArg@users.noreply.github.com>
2023-07-04 11:11:27 -0300

    solucionamos algunos errores de conexión 2

--- Java/Semana 12/SistemaEstudiantes/src/main/java/UTN/conexion/Conexion.java
+++ Java/Semana 12/SistemaEstudiantes/src/main/java/UTN/conexion/Conexion.java
@@ -8,7 +8,7 @@ public class Conexion {
     public static Connection getConnection(){
         Connection conexion = null;
         // variables para conectarnos a la base de datos
-        var baseDatos = "estudiantes2022";
+        var baseDatos = "estudiantes";
         var url = "jdbc:mysql://localhost:3306/"+baseDatos;
         var usuario = "root";
         var password = "admin";

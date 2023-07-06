64e4d5e8bbf872721e770baa223007bfb9aa2943
Angel Escobar <111505818+angesc2022@users.noreply.github.com>
2023-07-06 00:37:17 -0300

    Método agregarEstudiante() -> Parte 2
    tarea finalizada

--- Java/Semana 12/SistemaEstudiantes/src/main/java/UTN/datos/EstudianteDAO.java
+++ Java/Semana 12/SistemaEstudiantes/src/main/java/UTN/datos/EstudianteDAO.java
@@ -1,6 +1,6 @@
 package UTN.datos;
 
-import UTN.dominio.Estudiante;
+import static UTN.conexion.Conexion.getConnection;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -8,7 +8,7 @@ import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
 
-import static UTN.conexion.Conexion.getConnection;
+import UTN.dominio.Estudiante;
 
 public class EstudianteDAO {
     // Método listar
@@ -86,10 +86,21 @@ public class EstudianteDAO {
             ps.setString(2, estudiante.getApellido());
             ps.setString(3, estudiante.getTelefono());
             ps.setString(4, estudiante.getEmail());
-
+            ps.execute();
+            return true;
         } catch(Exception e){
             System.out.println("Ocurrió un error al agregar el estudiante: "+e.getMessage());
-        }
+        // Fin catch
+        finally{
+            try{
+                con.close();
+            }catch(Exception e){
+                System.out.println("error al cerrar la conexión: "+e.getMessage());
+        }// Fin catch
+    }//Finally
+        return false;
+
+        }// Fin método agregarEstudiante
     }
 
     public static void main(String[] args) {
